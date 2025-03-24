145860fa47db19f3233a21619e22ab35b21b7cbd
inatnun <natthanun@skooldio.tech>
2025-03-24 23:41:30 +0700

    update initial code for workshop

--- llm_handlers/basic_llm.py
+++ llm_handlers/basic_llm.py
@@ -1,10 +1,5 @@
 import streamlit as st
-from openai import OpenAI
-
-openai_api_key = st.secrets.llm.openai_api_key
-client = OpenAI(api_key=openai_api_key)
 
 
 def llm_response(messages):
-    response = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
-    return response.choices[0].message.content
+    return messages[len(messages) - 1]["content"]
